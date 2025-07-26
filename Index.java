5540274c11b27d078c4e1cea121e582533afd5e9
Priyanka In Tech <priyankabaravkar7373@gmail.com>
2025-07-26 11:37:48 +0530

    Add new code

---
+++ Index.java
@@ -0,0 +1,33 @@
+package devspace_demo;
+
+import java.util.Scanner;
+
+public class Index {
+    public static void main (String[]args){
+        System.out.println("Basic calculator");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number 1=");
+        int a=sc.nextInt();
+        System.out.print("Enter the number 2=");
+        int b=sc.nextInt();
+        System.out.print("Enter an oprater= (+ ,-,*,%)");
+        String operator=sc.next();
+
+        if(operator.equals("+")){
+            System.out.println(a+b);
+        }
+        else if (operator.equals("-")){
+            System.out.println(a-b);
+        }
+        else if (operator.equals("*")){
+            System.out.println(a*b);
+        }
+        else if (operator.equals("%")){
+            System.out.println(a%b);
+        }
+
+
+        
+    }
+}
